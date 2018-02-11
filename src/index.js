51d84f692eabe78706c5d0aac3e3bace009f2829
Scott moon <scottmoon@Scotts-MacBook-Pro.local>
2018-02-11 16:29:39 +0900

    코드 스플리팅 준비

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './client/Root';
+import { AppContainer } from 'react-hot-loader';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(
+  <AppContainer>
+    <Root />
+  </AppContainer>,
+  document.getElementById('root')
+)
+
+if(module.hot) {
+  module.hot.accept('./client/Root', () => {
+    const NextRoot = require('./client/Root').default;
+    ReactDOM.render(
+      <AppContainer>
+        <Root />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
+
 registerServiceWorker();
